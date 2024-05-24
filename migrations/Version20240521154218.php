2ce745039eb168a6cf29377e085187841dc84642
ClovisNorroy <clovis.norroy@gmail.com>
2024-05-24 18:22:15 +0200

    Add getGroceryList and saveGroceryList

--- migrations/Version20240521154218.php
+++ migrations/Version20240521154218.php
@@ -20,7 +20,7 @@ final class Version20240521154218 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE grocery_list (id INT AUTO_INCREMENT NOT NULL, list JSON DEFAULT NULL COMMENT \'(DC2Type:json)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE grocery_list (id INT AUTO_INCREMENT NOT NULL, list ARRAY DEFAULT NULL COMMENT \'(DC2Type:array)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
--- src/Controller/GroceryListController.php
+++ src/Controller/GroceryListController.php
@@ -2,18 +2,49 @@
 
 namespace App\Controller;
 
+use App\Entity\GroceryList;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GroceryListController extends AbstractController
 {
-    #[Route('/grocery/list', name: 'app_grocery_list')]
-    public function index(): JsonResponse
+    public function getGroceryList(EntityManagerInterface $entityManager): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/GroceryListController.php',
-        ]);
+        /**
+         * @param $groceryList GroceryList
+         */
+        /** @var \App\Entity\User $currentUser */
+        $currentUser = $this->getUser();
+        $groceryList = $currentUser->getGroceryLists()->first();
+        if($groceryList)
+            return $this->json($groceryList->getList());
+        else {
+            $newList = new GroceryList();
+            $newList->addUser($currentUser);
+            $entityManager->persist($newList);
+            $entityManager->flush();
+            return $this->json($newList->getList());
+        }
+            
+    }
+
+    public function saveGroceryList(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $newGroceryList = $request->toArray();
+        /**
+         * @param $groceryList GroceryList
+         */
+        /** @var \App\Entity\User $currentUser */
+        $currentUser = $this->getUser();
+        $groceryList = $currentUser->getGroceryLists()->first();
+        $groceryList->setList($newGroceryList);
+        $entityManager->persist($groceryList);
+        $entityManager->flush();
+
+        return new Response("Menu saved", 200);
     }
 }
